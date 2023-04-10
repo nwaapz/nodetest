858823c93d8c619b17c29b01501e88a69ab9d314
hosein abdl <53816663+nwaapz@users.noreply.github.com>
2023-04-10 17:57:08 +0430

    DBConfig

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+
+
+
+
+
+dbconnection.getConnection((err,connection)=> {
+   if(err)
+      throw err;
+   console.log('Database connected successfully');
+   connection.release();
+});
+
+
+
 const port=process.env.PORT || 3030;
 
 const WebSocket = require('ws')
